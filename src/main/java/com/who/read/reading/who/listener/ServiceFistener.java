4f969322d68361bbb659d0bc11203b68c3d23bcc
冯家栋的win10 <xiaoxi1598@163.com>
2020-06-17 22:36:56 +0800

    侦听器接口

---
+++ src/main/java/com/who/read/reading/who/listener/ServiceFistener.java
@@ -0,0 +1,11 @@
+package com.who.read.reading.who.listener;
+
+import com.who.read.reading.who.datamodel.Entity;
+
+/**
+ * Entity侦听器
+ */
+public interface ServiceFistener {
+
+    public void handle(Entity entity);
+}
